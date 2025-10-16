03f3748b167a36f0d5f08d1481bf7fde1132f2cc
RealCatDev <real.catdev@gmail.com>
2025-10-16 20:45:52 +0200

    [backend] Add Buffer

---
+++ inc/purrr/buffer.hpp
@@ -0,0 +1,30 @@
+#ifndef _PURRR_BUFFER_HPP_
+#define _PURRR_BUFFER_HPP_
+
+#include "purrr/object.hpp"
+namespace purrr {
+
+enum class BufferType {
+  Vertex,
+  Index
+};
+
+struct BufferInfo {
+  BufferType type;
+  size_t     size;
+};
+
+class Buffer : public purrr::Object {
+public:
+  Buffer()          = default;
+  virtual ~Buffer() = default;
+public:
+  Buffer(const Buffer &)            = delete;
+  Buffer &operator=(const Buffer &) = delete;
+public:
+  virtual void copy(size_t offset, size_t size, void *data) = 0;
+};
+
+} // namespace purrr
+
+#endif // _PURRR_BUFFER_HPP_
