1f25001e40bdef446eb08cb18b131f64bbb761f2
RealCatDev <real.catdev@gmail.com>
2025-10-17 14:19:37 +0200

    [purrr] Add Program include to purrr.hpp

--- inc/purrr/purrr.hpp
+++ inc/purrr/purrr.hpp
@@ -4,6 +4,7 @@
 #include "purrr/context.hpp" // IWYU pragma: export
 #include "purrr/window.hpp" // IWYU pragma: export
 #include "purrr/buffer.hpp" // IWYU pragma: export
+#include "purrr/program.hpp" // IWYU pragma: export
 
 namespace purrr {
 
